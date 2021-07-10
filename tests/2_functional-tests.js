5c1b95f59d216cd82ef9a8ee101e845135a6311e
Max Severin <makc.severin@gmail.com>
2021-07-10 20:34:41 +0000

    Add Functional Tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,4 +9,138 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 
+  suite("POST request to /api/translate", function () {   
+
+    test("Translation with text and locale fields", function (done) {
+      const data = {
+        locale: 'american-to-british',
+        text: 'Mangoes are my favorite fruit.',
+      };
+      const expected = 'Mangoes are my favourite fruit.';
+
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);  
+
+          assert.property(res.body, 'text');
+          assert.equal(res.body.text, data.text);
+
+          assert.property(res.body, 'translation');
+          assert.equal(res.body.translation, expected);
+
+          done();
+        });
+    });
+
+    test("Translation with text and invalid locale field", function (done) {
+      const data = {
+        locale: 'invalid-locale-field',
+        text: 'Mangoes are my favorite fruit.',
+      };
+      const error = 'Required field(s) missing';
+
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);  
+
+          assert.property(res.body, 'error');
+          assert.equal(res.body.error, error);
+
+          done();
+        });
+    });
+
+    test("Translation with missing text field", function (done) {
+      const data = {
+        locale: 'american-to-british',
+      };
+      const error = 'Required field(s) missing';
+
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);  
+
+          assert.property(res.body, 'error');
+          assert.equal(res.body.error, error);
+
+          done();
+        });
+    });
+
+    test("Translation with missing locale field", function (done) {
+      const data = {
+        text: 'Mangoes are my favorite fruit.',
+      };
+      const error = 'Required field(s) missing';
+
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);  
+
+          assert.property(res.body, 'error');
+          assert.equal(res.body.error, error);
+
+          done();
+        });
+    });
+
+    test("Translation with empty text", function (done) {
+      const data = {
+        locale: 'american-to-british',
+        text: '',
+      };
+      const error = 'Required field(s) missing';
+
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);  
+
+          assert.property(res.body, 'error');
+          assert.equal(res.body.error, error);
+
+          done();
+        });
+    });
+
+    test("Translation with text that needs no translation", function (done) {
+      const data = {
+        locale: 'british-to-american',
+        text: 'Mangoes are my favorite fruit.',
+      };
+      const expected = 'Everything looks good to me!';
+
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send(data)
+        .end(function (err, res) {
+          assert.equal(res.status, 200);  
+
+          assert.property(res.body, 'text');
+          assert.equal(res.body.text, data.text);
+
+          assert.property(res.body, 'translation');
+          assert.equal(res.body.translation, expected);
+
+          done();
+        });
+    });
+
+  });
+
 });
